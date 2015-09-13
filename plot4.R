020ef8143a35908f1472a1677625f739142b59bd
Jorge Oquendo <jorge.oquendo@eflglogal.com>
2015-09-12 22:59:35 -0500

    Add Graphic nro 4

--- plot4.R
+++ plot4.R
@@ -10,14 +10,15 @@ epcr <- epc[epc$Date == "2007-02-01" | epc$Date == "2007-02-02",]
 #make plot4.png
 par(mfrow = c(2,2))
 
+#First graphic
 par(mar = c(4,4, 2, 4))
 plot(epcr$DateTime, epcr$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
 
-
+#Second graphic
 par(mar = c(4,4, 2, 4))
 plot(epcr$DateTime, epcr$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
 
-
+#Third graphic
 par(mar = c(4,4, 2, 4))
 plot(epcr$DateTime, epcr$Sub_metering_1, type = "l", col = "black",xlab = "", ylab = "Energy sub metering")
 lines(epcr$DateTime, epcr$Sub_metering_2, col = "red")    
@@ -25,8 +26,10 @@ lines(epcr$DateTime, epcr$Sub_metering_3, col = "blue")
 legend("topright", lwd = 1, col = c("black", "red", "blue")
        , legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = .8, text.width = 100000, bty = "n")
 
+#Last grapchis
 par(mar = c(4,4, 2, 4))
 plot(epcr$DateTime, epcr$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
+
 
 dev.copy(png, file = "plot4.png")
 dev.off()
